26719cacc8fc83064879049b90d164ee29829f56
Daniel Harris <dan.j.harriss@gmail.com>
2017-03-26 11:23:50 +1030

    - Changed the order of the datapoints to be oldest to newest - Added some comments to show where JSON sets are opened and closed - Put the database queries into a serialize clause to solve an error where the second query (with the .end() call) finished before the first.

--- pv-json.js
+++ pv-json.js
@@ -3,8 +3,7 @@ var http = require('http');
 var sqlite3 = require('sqlite3').verbose();
 
 var smadata = "/home/dan/smadata/SBFspot.db";
-var inverter = "2130346114"; // Inverter serial number
-var port = 8001;
+var port = 8000;
 
 http.createServer(function(request, response) {
 	var db = new sqlite3.Database(smadata, sqlite3.OPEN_READONLY);
@@ -25,23 +24,26 @@ http.createServer(function(request, response) {
 
 	    response.statusCode = 200;
 	    response.setHeader('Content-Type', 'application/json');
-		response.write('{ "graph" : { "title" : "PV Output", "type" : "line", "yAxis" : { "minValue" : 0, "maxValue" : 4000 }, "datasequences" : [');
+		response.write('{ "graph" : { "title" : "Solar panels 5 min average output", "type" : "line", "yAxis" : { "minValue" : 0, "maxValue" : 4000, "units" : { "suffix" : "kW" } }, "datasequences" : [');
 		
-		db.all("SELECT strftime('%H:%M', TimeStamp) as title, power as value from vwDayData where TimeStamp like Date('now', '-1 day', 'localtime')||'%';", function(error, rows) {
-			response.write(' { "title" : "Yesterday", "color" : "orange", "datapoints" : ');
-			response.write(JSON.stringify(rows));
-			response.write('},');
-		});
+		db.serialize(function() {
+			db.all("SELECT strftime('%H:%M', TimeStamp) as title, power as value FROM vwDayData WHERE TimeStamp LIKE Date('now', '-1 day', 'localtime')||'%' ORDER BY title ASC;", function(error, rows) {
+				response.write(' { "title" : "Yesterday", "color" : "orange", "datapoints" : '); // open this set of datapoints, give it a title and colour
+				response.write(JSON.stringify(rows));
+				response.write('},'); // close this set of datapoints
+			});
 
-		db.all("SELECT strftime('%H:%M', TimeStamp) as title, power as value from vwDayData where TimeStamp like Date('now', 'localtime')||'%';", function(error, rows) {
-			response.write('{ "title" : "Today", "color" : "green", "datapoints" : ');
-			response.write(JSON.stringify(rows));
-			response.write('}]}}');
-			response.end() 
+			db.all("SELECT strftime('%H:%M', TimeStamp) as title, power as value FROM vwDayData WHERE TimeStamp LIKE Date('now', 'localtime')||'%' ORDER BY title ASC;", function(error, rows) {
+				response.write('{ "title" : "Yesterday", "color" : "orange", "datapoints" : ');  // open this set of datapoints, give it a title and colour
+				response.write(JSON.stringify(rows));
+				response.write('}'); // close this set of datapoints
+				response.write(']}}'); // close the set of dataseqiences, the graph, and the JSON data
+				response.end(); // tell the HTTP server this is the final block of data to be sent
+			});
 		});
 		
 		db.close();
 	});
-}).listen(port);
+}).listen(port, 'localhost');
 
 
